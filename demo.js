dc49aee79adbb20d3c52258e25d228d996bebec7
clonn <clonncd@gmail.com>
2013-03-20 20:46:27 +0800

    update demo.js

--- demo.js
+++ demo.js
@@ -1,7 +1,16 @@
 var vm = require('vm'),
     fs = require('fs'),
+    $ = require('jquery'),
     api = {};
 
 // read file from local nn sdk files
 // and pass parameter to api object
 vm.runInNewContext(fs.readFileSync('./nn-sdk.js', 'utf-8'), api);
+
+api.nn.initialize();
+
+var param = {
+};
+
+// then you can use nn sdk to fetch data.
+// ref: http://www.catchpo.tw/dev/doku.php
